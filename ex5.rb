9bdd639dbf296dd9a927ac515406ab4a96edf5fb
Christine <christinebeaubrun@gmail.com>
2016-02-17 20:41:11 -0800

    Initialize repository with Ruby exercises

---
+++ ex5.rb
@@ -0,0 +1,19 @@
+name = "Christine Beaubrun"
+age = 26
+height = 74
+weight = 110
+eyes = "Black"
+teeth = "White"
+hair = "Ombre"
+centimeters = 2.54
+kilograms = 2.2
+
+puts "Let's talk about #{name}"
+puts "She's #{height * centimeters} centimeters tall"
+puts "She's #{weight / kilograms} kilograms heavy"
+puts "Actually that's not too heavy"
+puts "She's got #{eyes} eyes and #{hair} hair."
+puts "Her teeth are always #{teeth}"
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
